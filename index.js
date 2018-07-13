ce86be3b09f2afe4aea0959b675c26fed04012a4
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-13 15:48:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   var divs = document.querySelectorAll('.ranked-list')
   
   for(let i=0; i<divs.length; i++) {
-    var lists = divs[i].getElementsByTagName('li')
+    var lists = divs[i].querySelectorAll('li')
     for(let j=0; j<lists.length; j++) {
       lists[j].innerHTML = (parseInt(lists[j].innerHTML) + n).toString()
     }
