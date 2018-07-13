d346113d00f505985909b02f0fc19ee9007c7c67
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-13 16:45:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  var divs = document.querySelectorAll('.ranked-list')
+  
+  for(let i=0; i<divs.length; i++) {
+    var lists = divs[i].querySelectorAll('li')
+    for(let j=0; j<lists.length; j++) {
+      lists[j].innerHTML = (parseInt(lists[j].innerHTML) + n).toString()
+    }
+  }
+}
+
+function deepestChild() {
+  var deepestNode = document.querySelector('#grand-node')
+  while(deepestNode.children.length>0) {
+    deepestNode = deepestNode.children[0]
+  }
+  return deepestNode
+}
