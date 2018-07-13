1e344e6741705c454b59aa45e7cda8373b886378
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-13 16:43:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,36 +19,8 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   var deepestNode = document.querySelector('#grand-node')
-  var depth = 0
-
-  for(let i=0; i<document.querySelector('#grand-node').children.length; i++) {
-    var curNode, curDepth = checkDepth(document.querySelector('#grand-node').children[i], 0)
-    if(curDepth > depth) {
-      depth = curDepth
-      deepestNode = curNode
-    }
+  while(deepestNode.children.length>0) {
+    deepestNode = deepestNode.children[0]
   }
-  
   return deepestNode
-}
-
-function checkDepth(node, depth) {
-  if(node === undefined) {
-    return node, depth
-  }
-  
-  var curNode = node
-  var curDepth = depth + 1
-  
-  var deepestNode = curNode
-  var deepestDepth = curDepth
-  
-  for(let i=0; i<node.children.length; i++) {
-    var childNode, childDepth = checkDepth(node.children[i], curDepth)
-    if(childDepth > deepestDepth) {
-      deepestDepth = childDepth
-      deepestNode = childNode
-    }
-  }
-  return deepestNode, deepestDepth
 }
